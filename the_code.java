0679d611c03293a7a3e9ffb1a3b22b2ffcc06bdb
WaterBear695264 <142440070+WaterBear695264@users.noreply.github.com>
2024-01-09 17:59:59 -0800

    Add files via upload

---
+++ the_code.java
@@ -0,0 +1,162 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
+
+import edu.wpi.first.wpilibj.XboxController;
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  private TalonFX mMotor;
+  private final int mMotorCanID = 10;
+  private XboxController mController;
+  private double currentEncoderPos;
+  private boolean IDontKnowWhatToCallThisVariable = true;
+  private int mMotorCurrentLimit = 20;
+  private final MotionMagicVoltage m_mmReq = new MotionMagicVoltage(0);  
+    /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    mMotor = new TalonFX(mMotorCanID);
+    mController = new XboxController(0);
+    SmartDashboard.putData("Auto choices", m_chooser);
+    TalonFXConfiguration configs = new TalonFXConfiguration();
+    configs.TorqueCurrent.PeakForwardTorqueCurrent = 10;
+    configs.TorqueCurrent.PeakReverseTorqueCurrent = -10;
+    StatusCode status = StatusCode.StatusCodeNotInitialized;
+    mMotor.setRotorPosition(0);
+    configs.Slot0.kP = 2.4;
+    configs.Slot0.kD = 0.1;
+    configs.Slot1.kP = 40;  
+    configs.Slot1.kD = 2;
+    for (int i = 0; i < 5; ++i) {
+      status = mMotor.getConfigurator().apply(configs);
+      if (status.isOK()) break;
+    }
+    if(!status.isOK()) {
+      System.out.println("Could not apply configs, error code: " + status.toString());
+    }
+  }
+
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+
+  }
+
+  /**
+   * This autonomous (along with the chooser code above) shows how to select between different
+   * autonomous modes using the dashboard. The sendable chooser code works with the Java
+   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
+   * uncomment the getString line to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
+   * below with additional strings. If using the SendableChooser make sure to add them to the
+   * chooser code above as well.
+   */
+  @Override
+  public void autonomousInit() {
+    m_autoSelected = m_chooser.getSelected();
+    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        // Put custom auto code here
+        break;
+      case kDefaultAuto:
+      default:
+        // Put default auto code here
+        break;
+    }
+  }
+
+  /** This function is called once when teleop is enabled. */
+  @Override
+  public void teleopInit() {}
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {
+    SmartDashboard.putNumber("Joystick X value", mController.getRightX());
+    // SmartDashboard.putNumber("motor Position", mMotor.getPosition());
+    System.out.println("motor position: " + mMotor.getPosition());
+    if(mController.getXButton()){
+      mMotor.set(0.1);
+    }
+    if(mController.getBButton()){
+      mMotor.set(0);
+    }
+    // if(mController.getXButton() && IDontKnowWhatToCallThisVariable){
+    //   currentEncoderPos = mMotor.getPosition().getValue();
+    //   IDontKnowWhatToCallThisVariable = false;
+    // }
+    // if(!IDontKnowWhatToCallThisVariable){
+    //   if(Math.abs((currentEncoderPos + 360) - (mMotor.getPosition().getValue())) < 10){
+    //     IDontKnowWhatToCallThisVariable = true;
+    //   }else{
+
+    //   }
+  //   }
+  }
+
+  /** This function is called once when the robot is disabled. */
+  @Override
+  public void disabledInit() {}
+
+  /** This function is called periodically when disabled. */
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This function is called once when test mode is enabled. */
+  @Override
+  public void testInit() {}
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {}
+
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {}
+}
